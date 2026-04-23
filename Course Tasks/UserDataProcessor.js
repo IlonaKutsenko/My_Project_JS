778fb44e9349310d2b36702988a2ff5ca8df771b
IlonaKutsenko <ilona.kutsenko@nixs.com>
2026-04-23 10:11:10 +0300

    Tasks: User and Shopping Cart Management User Data Processor Robust User Data Processor

---
+++ Course Tasks/UserDataProcessor.js
@@ -0,0 +1,34 @@
+const users = [
+  { name: 'Alice', age: 25, isActive: true },
+  { name: 'Bob', age: 17, isActive: true },
+  { name: 'Charlie', age: 70, isActive: false },
+  { name: 'David', age: 45, isActive: true },
+  { name: 'ADMIN', age: 99, isActive: true },
+  { name: 'Eve', age: 33, isActive: true }
+];
+
+function processUserData(userArray) {
+    let usersName = [];
+    
+    for (const user of userArray) { 
+        if (user.name === 'ADMIN') {
+            break;
+        }
+        if (user.isActive === false) {
+            continue;
+        }
+        if (user.age < 18) {
+            console.log(`${user.name} is a minor`);
+        }
+        else if (user.age >= 18 && user.age <= 65) {
+            console.log(`${user.name} is an adult`);
+        } 
+        else {
+            console.log(`${user.name} is a senior`);
+        }
+        usersName.push(user.name);
+    }
+    return usersName;
+}
+
+console.log(processUserData(users));
